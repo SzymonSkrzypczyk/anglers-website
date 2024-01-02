8aceb03b1424380c7eb6e0ee678649ca5abcee0f
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2024-01-02 18:33:24 +0100

    blog cos tam dodalem

---
+++ scripts/blog.js
@@ -0,0 +1,6 @@
+function sent(){
+    let text = document.getElementById("text_input").innerHTML;
+    let mail = document.getElementById("mail_input").innerHTML;
+    // narazie alert
+    alert("Yo");
+}
