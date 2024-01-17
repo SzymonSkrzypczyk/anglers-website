05463927a4e614145940111b7ff25a913d68eb9f
Szymon Skrzypczyk <77851583+SzymonSkrzypczyk@users.noreply.github.com>
2024-01-17 14:07:10 +0100

    poprawienie blogow i wysylanie maili

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,30 @@
 function sent(){
     let text = document.getElementById("text_input").innerHTML;
     let mail = document.getElementById("mail_input").innerHTML;
+    document.getElementById("text_input").value = "";
+    document.getElementById("mail_input").value = "";
     // narazie alert
-    alert("Yo");
+    //alert("An Email has been sent!");
+    const data = JSON.stringify({
+        ishtml: 'false',
+        sendto: mail,
+        title: 'Witamy na naszym portalu!',
+        body: `Dziekujemy za pozostawienie uwagi ${text}!`
+    });
+    
+    const xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
+    
+    xhr.addEventListener('readystatechange', function () {
+        if (this.readyState === this.DONE) {
+            console.log(this.responseText);
+        }
+    });
+    
+    xhr.open('POST', 'https://rapidmail.p.rapidapi.com/');
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('X-RapidAPI-Key', 'f9df9936a6msh0a23d1b6fe11df9p1426a1jsnfbe10dc0628f'); // nie powinno sie tego pokazywac, ale trudno XD
+    xhr.setRequestHeader('X-RapidAPI-Host', 'rapidmail.p.rapidapi.com');
+    
+    xhr.send(data);
 }
